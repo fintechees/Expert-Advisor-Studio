6c9bb886e6b7d90d9eefa46bf67e8bd52af9dd45
Fintechee <admin@fintechee.com>
2020-06-25 17:42:30 +0800

    Update indicator-plugin-for-MQL.js

--- EA/Plugin-for-MQL/indicator-plugin-for-MQL.js
+++ EA/Plugin-for-MQL/indicator-plugin-for-MQL.js
@@ -1,6 +1,6 @@
 	registerEA(
 		"mql_indicator_loader_plugin",
-		"mql_plugin to make MQL-based indicators runnable on Fintechee(v1.02)",
+		"mql_plugin to make MQL-based indicators runnable on Fintechee(v1.03)",
 		[{ // parameters
 			name: "definition",
 			value: "",
@@ -126,6 +126,34 @@
 								var arr = getDataFromIndi(obj.context, chartHandle, "Volume")
 								return arr[arr.length - shift - 1]
 							}, "iiii")
+							var jiHighest = Module.addFunction(function (uid, chartHandle, mode, count, start) {
+								var obj = window.mqlIndicatorsBuffer[uid + ""]
+								var md = window.mqlIndicators[obj.name].module.UTF8ToString(mode)
+								var arr = getDataFromIndi(obj.context, indiHandle, md)
+								var highest = -Number.MAX_VALUE
+								var idx = -1
+								for (var i = start; i < start + count && i >= 0 && i < arr.length; i++) {
+									if (arr[arr.length - i - 1] > highest) {
+										highest = arr[arr.length - i - 1]
+										idx = i
+									}
+								}
+								return idx
+							}, "iiiiii")
+							var jiLowest = Module.addFunction(function (uid, chartHandle, mode, count, start) {
+								var obj = window.mqlIndicatorsBuffer[uid + ""]
+								var md = window.mqlIndicators[obj.name].module.UTF8ToString(mode)
+								var arr = getDataFromIndi(obj.context, indiHandle, md)
+								var lowest = Number.MAX_VALUE
+								var idx = -1
+								for (var i = start; i < start + count && i >= 0 && i < arr.length; i++) {
+									if (arr[arr.length - i - 1] < lowest) {
+										lowest = arr[arr.length - i - 1]
+										idx = i
+									}
+								}
+								return idx
+							}, "iiiiii")
 							var jiACInit = Module.addFunction(function (uid, symbol, timeframe) {
 								var obj = window.mqlIndicatorsBuffer[uid + ""]
 								var symbolName = window.mqlIndicators[obj.name].module.UTF8ToString(symbol)
@@ -786,6 +814,8 @@
 								setjiClose: Module.cwrap('setjiClose', null, ['number']),
 								setjiVolumeInit: Module.cwrap('setjiVolumeInit', null, ['number']),
 								setjiVolume: Module.cwrap('setjiVolume', null, ['number']),
+								setjiHighest: Module.cwrap('setjiHighest', null, ['number']),
+								setjiLowest: Module.cwrap('setjiLowest', null, ['number']),
 								setjiACInit: Module.cwrap('setjiACInit', null, ['number']),
 								setjiAC: Module.cwrap('setjiAC', null, ['number']),
 								setjiADXInit: Module.cwrap('setjiADXInit', null, ['number']),
@@ -852,6 +882,8 @@
 							window.mqlIndicators[definition.name].setjiClose(jiClose)
 							window.mqlIndicators[definition.name].setjiVolumeInit(jiVolumeInit)
 							window.mqlIndicators[definition.name].setjiVolume(jiVolume)
+							window.mqlIndicators[definition.name].setjiHighest(jiHighest)
+							window.mqlIndicators[definition.name].setjiLowest(jiLowest)
 							window.mqlIndicators[definition.name].setjiACInit(jiACInit)
 							window.mqlIndicators[definition.name].setjiAC(jiAC)
 							window.mqlIndicators[definition.name].setjiADXInit(jiADXInit)
