76560fed93a0b95071b582b95ab23ab2d9907fbe
Fintechee <admin@fintechee.com>
2021-09-14 19:21:02 +0800

    Update toggle_trading_signals.js

--- Indicators/chart_elements/toggle_trading_signals.js
+++ Indicators/chart_elements/toggle_trading_signals.js
@@ -1,6 +1,18 @@
-	registerIndicator("toggle_trading_signals", "Toggle the display of the trading signals(v1.0)", function (context) {
+	registerIndicator("toggle_trading_signals", "Toggle the display of the trading signals(v1.02)", function (context) {
 	},[{
-		name: "opacity",
+		name: "opacityPendingOrder",
+		value: 0,
+		required: true,
+		type: PARAMETER_TYPE.NUMBER,
+		range: [0, 1.0]
+	},{
+		name: "opacityOpenTrade",
+		value: 0,
+		required: true,
+		type: PARAMETER_TYPE.NUMBER,
+		range: [0, 1.0]
+	},{
+		name: "opacityHistoryTrade",
 		value: 0,
 		required: true,
 		type: PARAMETER_TYPE.NUMBER,
@@ -22,27 +34,31 @@
 	}],
 	WHERE_TO_RENDER.CHART_WINDOW,
 	function (context) { // Init()
-		var opacity = getIndiParameter(context, "opacity")
-		var opacity2 = opacity > 0 ? 1 : 0;
+		var opacityPendingOrder = getIndiParameter(context, "opacityPendingOrder")
+		var opacityPendingOrder2 = opacityPendingOrder > 0 ? 1 : 0;
+		var opacityOpenTrade = getIndiParameter(context, "opacityOpenTrade")
+		var opacityOpenTrade2 = opacityOpenTrade > 0 ? 1 : 0;
+		var opacityHistoryTrade = getIndiParameter(context, "opacityHistoryTrade")
+		var opacityHistoryTrade2 = opacityHistoryTrade > 0 ? 1 : 0;
 		var isGlobal = getIndiParameter(context, "isGlobal")
 
 		var chartHandle = getChartHandleByContext(context)
 		var canvas = isGlobal ? d3 : d3.select("#cc_k_c_d_" + chartHandle)
 
-		canvas.selectAll(".cc_k_c_p_o_l").style("opacity", opacity2)
-		canvas.selectAll(".cc_k_c_p_o_t_l").style("opacity", opacity2)
-		canvas.selectAll(".cc_k_c_p_o_s_l").style("opacity", opacity2)
-		canvas.selectAll(".cc_k_c_p_o_b").style("opacity", opacity)
-		canvas.selectAll(".cc_k_c_p_o_t").style("opacity", opacity2)
-		canvas.selectAll(".cc_k_c_o_t_t_l").style("opacity", opacity2)
-		canvas.selectAll(".cc_k_c_o_t_s_l").style("opacity", opacity2)
-		canvas.selectAll(".cc_k_c_o_t_b").style("opacity", opacity)
-		canvas.selectAll(".cc_k_c_o_t_t").style("opacity", opacity2)
-		canvas.selectAll(".cc_k_c_h_t_o_b").style("opacity", opacity)
-		canvas.selectAll(".cc_k_c_h_t_c_b").style("opacity", opacity)
-		canvas.selectAll(".cc_k_c_h_t_o_t").style("opacity", opacity2)
-		canvas.selectAll(".cc_k_c_h_t_c_t").style("opacity", opacity2)
-		canvas.selectAll(".cc_k_c_h_t_l").style("opacity", opacity2)
+		canvas.selectAll(".cc_k_c_p_o_l").style("opacity", opacityPendingOrder2)
+		canvas.selectAll(".cc_k_c_p_o_t_l").style("opacity", opacityPendingOrder2)
+		canvas.selectAll(".cc_k_c_p_o_s_l").style("opacity", opacityPendingOrder2)
+		canvas.selectAll(".cc_k_c_p_o_b").style("opacity", opacityPendingOrder2)
+		canvas.selectAll(".cc_k_c_p_o_t").style("opacity", opacityPendingOrder2)
+		canvas.selectAll(".cc_k_c_o_t_t_l").style("opacity", opacityOpenTrade2)
+		canvas.selectAll(".cc_k_c_o_t_s_l").style("opacity", opacityOpenTrade2)
+		canvas.selectAll(".cc_k_c_o_t_b").style("opacity", opacityOpenTrade2)
+		canvas.selectAll(".cc_k_c_o_t_t").style("opacity", opacityOpenTrade2)
+		canvas.selectAll(".cc_k_c_h_t_o_b").style("opacity", opacityHistoryTrade2)
+		canvas.selectAll(".cc_k_c_h_t_c_b").style("opacity", opacityHistoryTrade2)
+		canvas.selectAll(".cc_k_c_h_t_o_t").style("opacity", opacityHistoryTrade2)
+		canvas.selectAll(".cc_k_c_h_t_c_t").style("opacity", opacityHistoryTrade2)
+		canvas.selectAll(".cc_k_c_h_t_l").style("opacity", opacityHistoryTrade2)
 	},
 	function (context) { // Deinit()
 		var opacity = 0.5
