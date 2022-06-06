16c44fd8077a0d219727ca3615c8667f233bca9b
Fintechee <admin@fintechee.com>
2022-06-06 11:07:28 +0800

    Create plugin_to_load_tensorflow.js

---
+++ EA/Plugin-for-Tensorflow/plugin_to_load_tensorflow.js
@@ -0,0 +1,125 @@
+registerEA(
+	  "plugin_to_load_tensorflow",
+	  "A plugin to load Tensorflow(v1.0)",
+	  [{ // parameters
+	    name: "tfjs",
+	    value: "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@2.0.0/dist/tf.min.js",
+	    required: true,
+	    type: PARAMETER_TYPE.STRING,
+	    range: null
+	  },{
+	    name: "tfvisjs",
+	    value: "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-vis",
+	    required: true,
+	    type: PARAMETER_TYPE.STRING,
+	    range: null
+	  }],
+	  function (context) { // Init()
+	    if (typeof tf == "undefined") {
+	      var tfjs = getEAParameter(context, "tfjs")
+	      var tfvisjs = getEAParameter(context, "tfvisjs")
+
+	      var tags = document.getElementsByTagName("script")
+	      for (var i = tags.length - 1; i >= 0; i--) {
+	        if (tags[i] && tags[i].getAttribute("src") != null && (tags[i].getAttribute("src") == tfjs || tags[i].getAttribute("src") == tfvisjs)) {
+	          tags[i].parentNode.removeChild(tags[i])
+	        }
+	      }
+
+	      var script1 = document.createElement("script")
+	      document.body.appendChild(script1)
+	      script1.onload = function () {
+	        var script2 = document.createElement("script")
+		      document.body.appendChild(script2)
+		      script2.onload = function () {
+	          window.buildCnn = function (featuresNum, kernelSize, filters, strides) {
+	            return new Promise(function (resolve, reject) {
+	              var tfModel = window.tf.sequential()
+
+	              tfModel.add(window.tf.layers.conv1d({
+	                inputShape: [featuresNum, 1],
+	                kernelSize: kernelSize,
+	                filters: filters,
+	                strides: strides,
+	                use_bias: true,
+	                activation: "relu",
+	                kernelInitializer: "VarianceScaling"
+	              }))
+
+	              tfModel.add(window.tf.layers.flatten({
+	              }))
+
+	              tfModel.add(window.tf.layers.dense({
+	                units: 2,
+	                kernelInitializer: "VarianceScaling",
+	                activation: "softmax"
+	              }))
+
+	              return resolve(tfModel)
+	            })
+	          }
+
+	          window.trainCnn = function (tfModel, trainingSet, epochs, batchSize, bMonitor) {
+	            if (bMonitor) {
+	              printMessage("Summary: ")
+	              tfModel.summary()
+	            }
+
+	            return new Promise(function (resolve, reject) {
+	              try {
+	                tfModel.compile({
+	                  optimizer: window.tf.train.adam(),
+	                  loss: "categoricalCrossentropy",
+	                  metrics: ["accuracy"]
+	                })
+
+	                if (bMonitor) {
+	                  tfModel.fit(trainingSet.input, trainingSet.output, {
+	                    batchSize: batchSize,
+	                    epochs: epochs,
+	                    shuffle: true,
+	                    callbacks: tfvis.show.fitCallbacks({
+	                      name: "Model Training", tab: "Model", styles: { height: "500px" }
+	                    }, ["loss", "val_loss", "acc", "val_acc"])
+	                  }).then(function (result) {
+	                    printMessage("Loss after last Epoch (" + result.epoch.length + ") is: " + result.history.loss[result.epoch.length-1])
+	                    resolve()
+	                  })
+
+	                  $("#tfjs-visor-container").show()
+	                } else {
+	                  tfModel.fit(trainingSet.input, trainingSet.output, {
+	                    batchSize: batchSize,
+	                    epochs: epochs,
+	                    shuffle: true
+	                  }).then(function (result) {
+	                    printMessage("Loss after last Epoch (" + result.epoch.length + ") is: " + result.history.loss[result.epoch.length-1])
+	                    resolve()
+	                  })
+	                }
+	              } catch (ex) {
+	                reject(ex)
+	              }
+	            })
+	          }
+
+	          popupMessage("Tensorflow has been loaded successfully!")
+	        }
+	        script2.onerror = function () {
+	          popupErrorMessage("Failed to load Tensorflow! Please run this plugin again.")
+	        }
+		      script2.async = true
+		      script2.src = tfvisjs
+	      }
+	      script1.onerror = function () {
+	        popupErrorMessage("Failed to load Tensorflow! Please run this plugin again.")
+	      }
+	      script1.async = true
+	      script1.src = tfjs
+	    }
+	  },
+	  function (context) { // Deinit()
+	  },
+	  function (context) { // OnTick()
+	  }
+	)
