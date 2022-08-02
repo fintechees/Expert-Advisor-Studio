93568e2b84affab0a0806777614132efe217199d
Fintechee <admin@fintechee.com>
2022-08-02 14:14:37 +0800

    Update unrealized_pl.js

--- Indicators/unrealized_pl.js
+++ Indicators/unrealized_pl.js
@@ -1,9 +1,14 @@
-registerIndicator("unrealized_pl", "An indicator to show the unrealized PL of the account(v1.01)", function (context) {
-  // Please note, this indicator can't be used independently. An EA called "unrealized_pl_viewer" that serves as an initiator should be used with this one simultaneously.
+registerIndicator("unrealized_pl", "An indicator to show the unrealized PL of the account(v1.02)", function (context) {
   var dataInput = getDataInput(context, 0)
   var dataOutputPl = getDataOutput(context, "totalPl")
   var dataOutputPlL = getDataOutput(context, "totalPlL")
   var dataOutputPlS = getDataOutput(context, "totalPlS")
+
+  if (typeof window.accounts != "undefined" && typeof window.accounts[context.chartHandle] != "undefined") {
+    context.totalPl = window.accounts[context.chartHandle].totalPl
+    context.totalPlL = window.accounts[context.chartHandle].totalPlL
+    context.totalPlS = window.accounts[context.chartHandle].totalPlS
+  }
 
   var calculatedLength = getCalculatedLength(context)
   var i = calculatedLength
@@ -55,7 +60,7 @@ function (context) { // init
     context.totalPlL = window.accounts[context.chartHandle].totalPlL
     context.totalPlS = window.accounts[context.chartHandle].totalPlS
   } else {
-    popupErrorMessage("You must define an window object in the source codes of your EA that tries to refer to this indicator to manage the information about the unrealized PL. Please specify chartHandle as the index number of the window object before getting the handle of this indicator.")
+    popupErrorMessage("Please note, this indicator can't be used independently. An EA called \"unrealized_pl_viewer\" that serves as an initiator should be used with this one simultaneously. You need to define an window object in the source codes of your EA that tries to refer to this indicator to manage the information about the unrealized PL. Please specify chartHandle as the index number of the window object before getting the handle of this indicator.")
   }
 },
 function (context) { // deinit
